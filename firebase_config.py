065d0182af07a01c986d0301b02b0415f345aa2f
Amrit Mandal <83923234+amritPVre@users.noreply.github.com>
2024-07-31 20:22:13 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,21 @@
-import firebase_admin
-from firebase_admin import credentials, db
+import json
 
-def initialize_firebase():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate("energy-eda-firebase-adminsdk-5v4ee-8b271f1a29.json")
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://energy-eda-default-rtdb.firebaseio.com/'
-        })
+# Initialize Firebase
+if not firebase_admin._apps:
+    firebase_config = {
+        "type": st.secrets["FIREBASE"]["type"],
+        "project_id": st.secrets["FIREBASE"]["project_id"],
+        "private_key_id": st.secrets["FIREBASE"]["private_key_id"],
+        "private_key": st.secrets["FIREBASE"]["private_key"].replace('\\n', '\n'),
+        "client_email": st.secrets["FIREBASE"]["client_email"],
+        "client_id": st.secrets["FIREBASE"]["client_id"],
+        "auth_uri": st.secrets["FIREBASE"]["auth_uri"],
+        "token_uri": st.secrets["FIREBASE"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["FIREBASE"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["FIREBASE"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["FIREBASE"]["universe_domain"]
+    }
+    cred = credentials.Certificate(firebase_config)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://energy-eda-default-rtdb.firebaseio.com'
+    })
