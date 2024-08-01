54b3ba4f87ff8ab04078484fa88df9115b9aa681
Amrit Mandal <83923234+amritPVre@users.noreply.github.com>
2024-08-01 23:45:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,29 @@ import base64
 from firebase_config import initialize_firebase
 from firebase_admin import db
 import xlsxwriter
+import firebase_admin
+from firebase_admin import credentials, db
+
+
+# Initialize Firebase
+if not firebase_admin._apps:
+    firebase_config = {
+        "type": st.secrets["FIREBASE"]["type"],
+        "project_id": st.secrets["FIREBASE"]["project_id"],
+        "private_key_id": st.secrets["FIREBASE"]["private_key_id"],
+        "private_key": st.secrets["FIREBASE"]["private_key"].replace('\\n', '\n'),
+        "client_email": st.secrets["FIREBASE"]["client_email"],
+        "client_id": st.secrets["FIREBASE"]["client_id"],
+        "auth_uri": st.secrets["FIREBASE"]["auth_uri"],
+        "token_uri": st.secrets["FIREBASE"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["FIREBASE"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["FIREBASE"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["FIREBASE"]["universe_domain"]
+    }
+    cred = credentials.Certificate(firebase_config)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://energy-eda-default-rtdb.firebaseio.com'
+    })
 
 
 # Streamlit app layout
