cd27d0f72549780ca0e63dfc11463cb74710202c
Amrit Mandal <83923234+amritPVre@users.noreply.github.com>
2024-08-01 23:49:21 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,22 +3,20 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 
-# Initialize Firebase
-if not firebase_admin._apps:
-    firebase_config = {
-        "type": st.secrets["FIREBASE"]["type"],
-        "project_id": st.secrets["FIREBASE"]["project_id"],
-        "private_key_id": st.secrets["FIREBASE"]["private_key_id"],
-        "private_key": st.secrets["FIREBASE"]["private_key"].replace('\\n', '\n'),
-        "client_email": st.secrets["FIREBASE"]["client_email"],
-        "client_id": st.secrets["FIREBASE"]["client_id"],
-        "auth_uri": st.secrets["FIREBASE"]["auth_uri"],
-        "token_uri": st.secrets["FIREBASE"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["FIREBASE"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["FIREBASE"]["client_x509_cert_url"],
-        "universe_domain": st.secrets["FIREBASE"]["universe_domain"]
-    }
-    cred = credentials.Certificate(firebase_config)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://energy-eda-default-rtdb.firebaseio.com'
-    })
+def initialize_firebase():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate({
+            "type": st.secrets["firebase"]["type"],
+            "project_id": st.secrets["firebase"]["project_id"],
+            "private_key_id": st.secrets["firebase"]["private_key_id"],
+            "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+            "client_email": st.secrets["firebase"]["client_email"],
+            "client_id": st.secrets["firebase"]["client_id"],
+            "auth_uri": st.secrets["firebase"]["auth_uri"],
+            "token_uri": st.secrets["firebase"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+        })
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://your_project_id.firebaseio.com/'
+        })
